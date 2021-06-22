6cbf0c2e3b42b802baed9b0660b9bd4050a136c1
Jose Rodriguez <isc.joserodriguez@gmail.com>
2021-06-22 16:47:44 -0600

    [App] Server Refactorized

--- app.js
+++ app.js
@@ -4,33 +4,12 @@ const express = require('express'),
     cors = require('cors'),
     app = express(),
     fetch = require('node-fetch'),
-    mcache = require('memory-cache');
+    cache = require('./middlewares');
 
 
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-
-const cache = (duration) => (req, res, next) => {
-    let key = '__express__' + req.originalUrl || req.url;
-    let cachedBody = mcache.get(key);
-    if (cachedBody) {
-        let response=JSON.parse(cachedBody);
-        if(response.error){
-            res.status(400).json(response);
-        }else{
-            res.status(200).json(response);
-        }
-        return;
-    } else {
-        res.sendResponse = res.send;
-        res.send = (body) => {
-            mcache.put(key, body, duration * 1000);
-            res.sendResponse(body);
-        }
-        next();
-    }
-}
 
 app.get('/', (req, res) => {
     res.status(200).json({ message: 'Hello World!' });
